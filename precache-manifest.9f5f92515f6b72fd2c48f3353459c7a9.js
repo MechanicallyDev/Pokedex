8e373aad8fc16d8c2872a55635bbef318194bddd
MechanicallyDev <contato@mechanically.dev>
2020-09-11 16:12:39 -0300

    Updates

---
+++ precache-manifest.9f5f92515f6b72fd2c48f3353459c7a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b582aab095a8b84ecb52afa68c5a20a",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "170af55f35530e066a2c",
+    "url": "/pokedex/static/css/main.5e446d85.chunk.css"
+  },
+  {
+    "revision": "0ac43369cac3766d19fe",
+    "url": "/pokedex/static/js/2.7b8b1839.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.7b8b1839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "170af55f35530e066a2c",
+    "url": "/pokedex/static/js/main.460740e6.chunk.js"
+  },
+  {
+    "revision": "c79ff3590a8e40397a6a",
+    "url": "/pokedex/static/js/runtime-main.d1e0ff4a.js"
+  }
+]);
